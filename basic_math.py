f86ea9f638d0f6b024e1f35cba555b02cbd88485
Shem Irekpita <shem.irekpita@stu.ocsb.ca>
2025-09-15 18:19:20 +0000

    “message”

---
+++ basic_math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Created By: Shem
+# Date: 9/15/2025
+# Write what this program does.
+import math
+
+
+def main():
+    print(5 + 5)
+    print(5 - 8)
+    print(7 * 8)
+    print(9**10)
+    print(math.sqrt(40))
+
+
+if __name__ == "__main__":
+    main()
